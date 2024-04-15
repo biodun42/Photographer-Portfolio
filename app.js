de2042fe7870b4b7410853a98f31a363ad4a3468
Muhammed Abiodun <muhammedabiodun42@gmail.com>
2024-04-15 17:04:42 +0100

    commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const faqButton = document.querySelectorAll(".faq-button");
 const back = document.querySelectorAll(".back");
 const front = document.querySelectorAll(".front");
 const body = document.querySelector(".container");
+const loader = document.querySelector(".preloader");
 
 
 for (let i = 0; i < faqButton.length; i++) {
@@ -54,3 +55,6 @@ navs.forEach((nav)=>{
   })
 })
 
+window.onload = function(){
+  loader.style.display = 'none'
+}
